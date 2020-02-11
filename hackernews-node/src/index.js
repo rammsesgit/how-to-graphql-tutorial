4cb08bac99f65016c44d78a3279f863949046d9e
Isar Ramses <rammses_@live.com.mx>
2020-02-11 13:08:24 -0600

    A Simple Query

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,20 +1,44 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
-// 2
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  },
+  {
+    id: 'link-1',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+]
+
 const resolvers = {
   Query: {
-    info: () => null
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  // implementation of the Link resolvers is trivial
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers
