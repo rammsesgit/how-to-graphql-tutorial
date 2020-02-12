22374c61bcc3f99edf19cba65d134b28e490bb99
Isar Ramses <rammses_@live.com.mx>
2020-02-11 18:20:58 -0600

    A Simple Mutation

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,46 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
 let links = [
   {
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
-  },
-  {
-    id: 'link-1',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
   }
 ]
+
+let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  // implementation of the Link resolvers is trivial
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
