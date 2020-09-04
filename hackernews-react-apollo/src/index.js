038994598270bec6d659f5ebf070e466917c39b1
Isar Ramses <rammses_@live.com.mx>
2020-09-04 18:57:57 -0500

    Mutations: Creating Links

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -6,18 +6,33 @@ import * as serviceWorker from './serviceWorker'
 
 // 1
 import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
+/* import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { InMemoryCache } from 'apollo-cache-inmemory' */
+
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 // 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
